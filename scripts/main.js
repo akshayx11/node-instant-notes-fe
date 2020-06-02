a55bec7b04d1c6703049a0d7491fdb7154aaed0e
akshayx11 <akshayx11@gmail.com>
2020-06-03 01:10:11 +0530

    First commit

---
+++ scripts/main.js
@@ -0,0 +1,36 @@
+$(document).ready(async () => {
+    let textContents = ``;
+    let dataUrl = "https://node-insta-notes.herokuapp.com/";
+    $.get(dataUrl).done(( data ) => {
+      const keys = Object.keys(data);
+      console.log("KEYS");
+      for(const key of keys) {
+          const { content = "" } = data[`${key}`];
+          textContents = `<li keyid="${key}"><div class="deleteContent" keyid="${key}">X</div>${content}</li>`+textContents;
+      }
+      $("#content-list").html(`<ol reversed>${textContents}</ol>`);
+    });
+     
+      
+    // add note
+    $("#addContent").submit((event) => {
+        event.preventDefault();
+        content = $("#newText").val();
+        let posting = $.post( dataUrl, { content } );
+        posting.done(function( data ) {
+            $("#content-list ol").prepend(`<li keyid="${data.addedDate}"><div class="deleteContent" keyid="${data.addedDate}">X</div><pre>${data.content}</pre></li>`);
+            $("#newText").val("");
+        });
+   });
+   //delete note
+    
+     $(document.body).on('click', '.deleteContent', (event) =>{
+        const keyid = $(event.currentTarget).attr('keyid');
+        $.ajax({
+            type: "DELETE",
+            url: `${dataUrl}/${keyid}`,
+        });
+        $(event.currentTarget).parent().hide();
+     });
+   
+});
